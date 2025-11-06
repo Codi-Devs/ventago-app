a33bdfb6e1122c0debf0b91d90d7cb26983770a2
Oegh <oegh1296@gmail.com>
2025-11-05 23:05:30 -0500

    UI fixes

--- composeApp/src/commonMain/kotlin/com/teco/ventago/features/payments/ui/yappy/YappyScreen.kt
+++ composeApp/src/commonMain/kotlin/com/teco/ventago/features/payments/ui/yappy/YappyScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -168,6 +169,7 @@ fun YappyScreenView(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .imePadding()
             .verticalScroll(rememberScrollState())
     ) {
 
