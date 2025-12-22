8dbaabfc7fe6b135ba49645e59e11e0fd9951633
Oegh <oegh1296@gmail.com>
2025-12-22 15:26:44 -0500

    Last fixes before Live

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -113,8 +113,8 @@ android {
         applicationId = "com.teco.ventago"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 11
-        versionName = "1.1.2"
+        versionCode = 12
+        versionName = "1.1.3"
     }
     packaging {
         resources {
