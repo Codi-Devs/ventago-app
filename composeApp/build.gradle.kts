bd456a5fa3fde25e1a58edfa277978d4c2443b26
Oegh <oegh1296@gmail.com>
2026-01-20 16:58:36 -0500

    Add quotes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -114,8 +114,8 @@ android {
         applicationId = "com.teco.ventago"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 15
-        versionName = "1.2.0"
+        versionCode = 16
+        versionName = "1.2.1"
     }
     packaging {
         resources {
