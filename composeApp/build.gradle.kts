91be26e87c45825c71ae224c7c05d5577d111be2
Oegh <oegh1296@gmail.com>
2026-02-12 22:38:12 -0500

    Fix locations

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -114,8 +114,8 @@ android {
         applicationId = "com.teco.ventago"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 23
-        versionName = "1.3.1"
+        versionCode = 24
+        versionName = "1.3.2"
     }
     packaging {
         resources {
