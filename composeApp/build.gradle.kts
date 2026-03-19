8a0c4589f8febeca0c666017071f0b85d702b7bb
Oegh <oegh1296@gmail.com>
2026-03-19 17:53:59 -0500

    Update version code and version name to 33 and 1.4.1 respectively

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -114,8 +114,8 @@ android {
         applicationId = "com.teco.ventago"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 32
-        versionName = "1.4.0"
+        versionCode = 33
+        versionName = "1.4.1"
     }
     packaging {
         resources {
