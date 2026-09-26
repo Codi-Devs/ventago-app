0acd8df00707788d91b077e1dfa8300ef0735793
Oegh <oegh1296@gmail.com>
2026-09-26 01:19:51 -0500

    1.6.7

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -111,8 +111,8 @@ android {
         applicationId = "com.teco.ventago"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 52
-        versionName = "1.6.6"
+        versionCode = 53
+        versionName = "1.6.7"
         buildConfigField("boolean", "IS_POS_BUILD", "false")
     }
 
